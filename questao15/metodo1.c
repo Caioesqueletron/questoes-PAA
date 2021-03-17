6d7f639b4dff67222719d2eec5e713152ce75c3d
caioesqueletron <caiocaio620@gmail.com>
2021-03-16 23:30:33 -0300

    update

--- questao15/metodo1.c
+++ questao15/metodo1.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int maxsimum(int a[], int l, int r)
+int Valor(int a[], int l, int r)
 {
     if (r - l == 1)
         return a[l];
     int m = (l + r) / 2;
-    int u = maxsimum(a, l, m);
-    int v = maxsimum(a, m, r);
+    int u = Valor(a, l, m);
+    int v = Valor(a, m, r);
     return u > v ? u : v;
 }
 int main()
@@ -16,7 +16,7 @@ int main()
     int a[] = {5, 8, 40, 30, 20, 40, 50, 10, 17, 51};
     int n = sizeof(a) / sizeof(int);
 
-    printf("%d\n",maxsimum(a, 0, n));
+    printf("%d\n",Valor(a, 0, n));
 
     return 0;
 }
